b0b7b91ed0ab32da422e689f6740f0372929d138
samuelmodlin <modlin.samuel@gmail.com>
2020-10-27 12:13:05 -0700

    First commit

---
+++ client/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+    typography: {
+      useNextVariants: true,
+    },
+    palette: {
+      primary: {
+        main: '#d32f2f',
+      }
+    }
+  })
+
+  export default theme
