11a29f5e8e5a6c6e4a216caa36502ce69f61668d
muffin <Berl@BROADWAY.local>
2020-11-07 23:05:48 -0500

    so turns out im really bad at this and this is a mess

--- client/pages/Test.js
+++ client/pages/Test.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Button from '@material-ui/core/Button'
 import { withStyles } from '@material-ui/core/styles';
 import { makeStyles } from '@material-ui/core/styles';
-import Accordion from '@material-ui/core/Accordion';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+//import Accordion from '@material-ui/core/Accordion';
+//import AccordionDetails from '@material-ui/core/AccordionDetails';
+//import AccordionSummary from '@material-ui/core/AccordionSummary';
+//import Typography from '@material-ui/core/Typography';
+//import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const useStyles = theme => ({
     exampleCss: {
@@ -14,18 +14,16 @@ const useStyles = theme => ({
     },
 });
 
+export default function SimpleAccordion() {
+  const classes = useStyles();
+
 class Test extends React.Component {
     render() {
         const { classes } = this.props;
 
         return (
-            <div>
-                <Accordion>Accordion MUI</Accordion>
-                //not sure what im supposed to put in the line under this
-                <p className={classes.exampleCss}>Example JSX: Test Page</p>
-                return (
-                  <div className={classes.root}>
-                    <Accordion>
+              <div className={classes.root}>
+                 <Accordion>
                       <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel1a-content"
@@ -33,16 +31,21 @@ class Test extends React.Component {
                       >
                         <Typography className={classes.heading}>Test 1</Typography>
                       </AccordionSummary>
-                      <AccordionDetails>
-                        <Typography>
-                          //Text: how can we make it so they can input the text?
-                        </Typography>
-                      </AccordionDetails>
+                    //  <AccordionDetails>
+                      //  <Typography>
+                          //<AddItem
+                          //    name={name}
+                          //    />
+                    //    </Typography>
+                    //  </AccordionDetails>
+                    >
                     </Accordion>
+                    // not sure what im supposed to put in the line under this
+                    <p className={classes.exampleCss}>Example JSX: Test Page</p>
             </div>
         )
+
     }
 }
-
 
 export default withStyles(useStyles)(Test)
