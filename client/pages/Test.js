9402ba4b637751f9be3a9ab400c467af297d027f
Grace  Coleman <gracecoleman@graces-mbp-3.wireless.davidson.edu>
2020-11-08 14:15:45 -0500

    fixing error

--- client/pages/Test.js
+++ client/pages/Test.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
-import { withStyles } from '@material-ui/core/styles';
-import { makeStyles } from '@material-ui/core/styles';
+import {
+  withStyles
+} from '@material-ui/core/styles';
+import {
+  makeStyles
+} from '@material-ui/core/styles';
 //import Accordion from '@material-ui/core/Accordion';
 //import AccordionDetails from '@material-ui/core/AccordionDetails';
 //import AccordionSummary from '@material-ui/core/AccordionSummary';
@@ -9,43 +13,57 @@ import { makeStyles } from '@material-ui/core/styles';
 //import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const useStyles = theme => ({
-    exampleCss: {
-        margin: theme.spacing(2),
-    },
+  exampleCss: {
+    margin: theme.spacing(2),
+  },
 });
 
-export default function SimpleAccordion() {
-  const classes = useStyles();
+// export default function SimpleAccordion() {
+//   const classes = useStyles();
 
 class Test extends React.Component {
-    render() {
-        const { classes } = this.props;
+  render() {
+    const {
+      classes
+    } = this.props;
 
-        return (
-              <div className={classes.root}>
-                 <Accordion>
-                      <AccordionSummary
-                        expandIcon={<ExpandMoreIcon />}
-                        aria-controls="panel1a-content"
-                        id="panel1a-header"
-                      >
-                        <Typography className={classes.heading}>Test 1</Typography>
-                      </AccordionSummary>
-                    //  <AccordionDetails>
-                      //  <Typography>
-                          //<AddItem
-                          //    name={name}
-                          //    />
-                    //    </Typography>
-                    //  </AccordionDetails>
-                    >
-                    </Accordion>
-                    // not sure what im supposed to put in the line under this
-                    <p className={classes.exampleCss}>Example JSX: Test Page</p>
-            </div>
-        )
+    return ( <
+      div className = {
+        classes.root
+      } >
+      <
+      Accordion >
+      <
+      AccordionSummary expandIcon = {
+        <
+        ExpandMoreIcon / >
+      }
+      id = "panel1a-header" >
+      <
+      Typography className = {
+        classes.heading
+      } > Test 1 < /Typography> < /
+      AccordionSummary > <
+      AccordionDetails >
+      <
+      Typography >
+      <
+      AddItem name = {
+        name
+      }
+      /> < /
+      Typography > <
+      /AccordionDetails> > < /
+      Accordion >
+      // not sure what im supposed to put in the line under this
+      <
+      p className = {
+        classes.exampleCss
+      } > Example JSX: Test Page < /p> < /
+      div >
+    )
 
-    }
+  }
 }
 
 export default withStyles(useStyles)(Test)
